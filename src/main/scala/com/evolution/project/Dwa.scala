4ce242197bf004a560e12d3b406f4baf67267d61
Filip Pawlak <filippawlak@gmail.com>
2022-07-05 11:31:52 +0200

    Added my project

---
+++ src/main/scala/com/evolution/project/Dwa.scala
@@ -0,0 +1,7 @@
+package com.evolution.project
+
+case class Costam(foo: String, bar: String)
+
+object Dwa extends App {
+  println("hello2")
+}
---
+++ src/main/scala/com/evolution/project/Main.scala
@@ -0,0 +1,23 @@
+package com.evolution.project
+
+// import cats.effect.{ExitCode, IO, IOApp}
+// import org.http4s.{HttpApp, HttpRoutes, Response}
+// import org.http4s.server.blaze.BlazeServerBuilder
+// import org.http4s.dsl.io._
+// import org.http4s.implicits._
+
+// import scala.concurrent.ExecutionContext
+// import cats.effect.concurrent.Ref
+
+// import java.time.Instant
+
+// import cats.syntax.either._
+// import io.circe._
+// import io.circe.Decoder.Result
+// import io.circe.generic.JsonCodec
+// import io.circe.parser._
+// import io.circe.syntax._
+
+object Main extends App {
+  println("Hello, World!")
+}
